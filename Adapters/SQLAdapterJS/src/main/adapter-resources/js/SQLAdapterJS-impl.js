dcd05b190065f4a3178cd9b57c87972c857f53e6
Shmulik Bardosh <SHMULIKB@il.ibm.com>
2016-05-17 15:00:29 +0300

    Beautify code + manipulate the returned value of procedures

--- Adapters/SQLAdapterJS/src/main/adapter-resources/js/SQLAdapterJS-impl.js
+++ Adapters/SQLAdapterJS/src/main/adapter-resources/js/SQLAdapterJS-impl.js
@@ -1,18 +1,18 @@
 /**
-* Copyright 2015 IBM Corp.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 //****************************************
 // getCurrenciesList
@@ -20,10 +20,11 @@
 var getCurrenciesListStatement = "SELECT id, symbol, name FROM currencies;";
 
 function getCurrenciesList() {
-	return MFP.Server.invokeSQLStatement({
-		preparedStatement : getCurrenciesListStatement,
-		parameters : []
-	});
+    var list = MFP.Server.invokeSQLStatement({
+        preparedStatement: getCurrenciesListStatement,
+        parameters: []
+    });
+    return list.resultSet;
 }
 
 //****************************************
@@ -32,31 +33,36 @@ function getCurrenciesList() {
 var getCurrencySymbolStatement = "SELECT symbol FROM currencies where id=?;";
 
 function getCurrencySymbol(currency_id) {
-	return MFP.Server.invokeSQLStatement({
-		preparedStatement : getCurrencySymbolStatement,
-		parameters: [currency_id]
-	});
+    var queryResult = MFP.Server.invokeSQLStatement({
+        preparedStatement: getCurrencySymbolStatement,
+        parameters: [currency_id]
+    });
+    return queryResult.resultSet[0].symbol;
+
 }
 
 //****************************************
 // getExchangeRate
 //****************************************
-function getExchangeRate(fromId, toId){
-    base = getCurrencySymbol(fromId).resultSet[0].symbol;
-    exchangeTo = getCurrencySymbol(toId).resultSet[0].symbol;
-		ExchangeRate = null;
+function getExchangeRate(fromId, toId) {
+    var base = getCurrencySymbol(fromId);
+    var exchangeTo = getCurrencySymbol(toId);
+    var ExchangeRate = null;
 
-		if(base == exchangeTo){
-			ExchangeRate = 1;
-		}
-		else {
-			var fixerExchangeRateJSON =  MFP.Server.invokeProcedure({
-	    	adapter : 'HTTPAdapterJS',
-	    	procedure : 'getExchangeRate',
-	    	parameters : [base, exchangeTo]
-	    });
-			ExchangeRate = fixerExchangeRateJSON.rates[exchangeTo];
-		}
+    if (base == exchangeTo) {
+        ExchangeRate = 1;
+    } else {
+        var fixerExchangeRateJSON = MFP.Server.invokeProcedure({
+            adapter: 'HTTPAdapterJS',
+            procedure: 'getExchangeRate',
+            parameters: [base, exchangeTo]
+        });
+        ExchangeRate = fixerExchangeRateJSON.rates[exchangeTo];
+    }
 
-    return {"base":base, "target":exchangeTo, "exchangeRate":ExchangeRate};
+    return {
+        "base": base,
+        "target": exchangeTo,
+        "exchangeRate": ExchangeRate
+    };
 }
